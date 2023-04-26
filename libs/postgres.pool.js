af93356b2b6accf5cec84e8cda6ca6afddbcaefb
Anthony Matheus <antyoma33@gmail.com>
2023-04-26 16:01:14 -0500

    ordenando las variables de entorno

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-    host: 'localhost',
-    port: 5432,
-    user: 'antyoma',
-    password: 'admin123',
-    database: 'my_store'
-});
+const { config } = require('./../config/config');
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbNmae}`;
+
+const pool = new Pool({ connectionString: URI });
 
 module.exports = pool;
