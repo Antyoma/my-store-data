60920747005b6a867cc70e5ae16b3aa7f6cf4167
Anthony Matheus <antyoma33@gmail.com>
2023-05-08 17:45:57 -0500

    Ordenes de compra y paginación

--- routes/orders.router.js
+++ routes/orders.router.js
@@ -5,13 +5,15 @@ const validatorHandler = require('../middlewares/validator.handler');
 const {
   getOrderSchema,
   createOrderSchema,
+  addItemSchema,
 } = require('../schemas/order.schema');
 
 const router = express.Router();
 const service = new OrderService();
 
-router.get('/:id',
-validatorHandler(getOrderSchema, 'params'),
+router.get(
+  '/:id',
+  validatorHandler(getOrderSchema, 'params'),
   async (req, res, next) => {
     try {
       const { id } = req.params;
@@ -23,13 +25,28 @@ validatorHandler(getOrderSchema, 'params'),
   }
 );
 
-router.post('/',
-validatorHandler(createOrderSchema, 'body'),
+router.post(
+  '/',
+  validatorHandler(createOrderSchema, 'body'),
   async (req, res, next) => {
     try {
       const body = req.body;
       const newOrder = await service.create(body);
       res.status(201).json(newOrder);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post(
+  '/add-item',
+  validatorHandler(addItemSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newItem = await service.addItem(body);
+      res.status(201).json(newItem);
     } catch (error) {
       next(error);
     }
